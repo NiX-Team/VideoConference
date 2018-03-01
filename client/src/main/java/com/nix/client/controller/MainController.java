031afa5e3d0dcf71f6286caa9116ecf6f3f31920
DESKTOP-2BJDEOE\11723 <1172304645@qq.com>
2018-03-01 22:16:38 +0800

    添加后台项目

---
+++ client/src/main/java/com/nix/client/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.nix.client.controller;
+
+import javafx.scene.input.MouseEvent;
+
+/**
+ * @author 11723
+ * 主控制面板
+ */
+public class MainController {
+
+    public void setImage(MouseEvent mouseEvent) {
+
+    }
+}
